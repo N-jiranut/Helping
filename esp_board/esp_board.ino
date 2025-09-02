ad1cad1a8c7795596b3d01593b72dda249457e95
N-jiranut <jiranut.nat@gmail.com>
2025-09-02 20:28:58 +0700

    z

---
+++ esp_board/esp_board.ino
@@ -0,0 +1,157 @@
+#include <Wire.h>
+#include <Adafruit_MPU6050.h>
+#include <Adafruit_Sensor.h>
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+const char* ssid = "vivo_V25";
+const char* password = "02_jiranut";
+
+// const String scriptURL = "https://script.google.com/macros/s/AKfycbzBV-m5zoYQ2tLPSUkq4XFDHVlQaisYEhMwOvvxOY4EFUd07wML1OWgW_mMngrxlU7iaQ/exec"; // ลิงก์ Web App
+const char* serverName = "http://10.207.14.79:5000/data";
+
+WiFiClient client;
+HTTPClient http;
+
+void connectToWiFi() {
+  Serial.print("Connecting to WiFi...");
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500); 
+    Serial.print(".");
+  }
+  Serial.println("\nConnected to WiFi!");
+}
+
+Adafruit_MPU6050 mpu;
+
+const int FLEX_PIN_THUMB = 1;
+const int FLEX_PIN_INDEX = 2;
+const int FLEX_PIN_MIDDLE = 3;
+const int FLEX_PIN_RING = 4;
+const int FLEX_PIN_PINKY = 5;
+
+int flexSensorValues[5];
+const int FLEX_PINS[] = {FLEX_PIN_THUMB, FLEX_PIN_INDEX, FLEX_PIN_MIDDLE, FLEX_PIN_RING, FLEX_PIN_PINKY};
+
+const int FLEX_MIN_STRAIGHT = 2000;
+const int FLEX_MAX_BENT = 3500;
+
+void setup() {
+  Serial.begin(115200);
+  delay(1000);
+
+  connectToWiFi(); 
+  Serial.println("Initializing I2C bus on GPIO 8 (SDA) and GPIO 9 (SCL)...");
+  Wire.begin(8, 9);
+  delay(100);
+  Serial.println("Attempting to find MPU6050 chip...");
+  if (!mpu.begin()) {
+    Serial.println("Failed to find MPU6050 chip. Check wiring, power, and I2C address.");
+    while (1) {
+      delay(10);
+    }
+  }
+  Serial.println("MPU6050 Found and initialized!");
+  mpu.setAccelerometerRange(MPU6050_RANGE_8_G);
+  Serial.print("Accelerometer range set to: ");
+  switch (mpu.getAccelerometerRange()) {
+    case MPU6050_RANGE_2_G: Serial.println("+-2G"); break;
+    case MPU6050_RANGE_4_G: Serial.println("+-4G"); break;
+    case MPU6050_RANGE_8_G: Serial.println("+-8G"); break;
+    case MPU6050_RANGE_16_G: Serial.println("+-16G"); break;
+  }
+
+  mpu.setGyroRange(MPU6050_RANGE_500_DEG);
+  Serial.print("Gyro range set to: ");
+  switch (mpu.getGyroRange()) {
+    case MPU6050_RANGE_250_DEG: Serial.println("+-250 deg/s"); break;
+    case MPU6050_RANGE_500_DEG: Serial.println("+-500 deg/s"); break;
+    case MPU6050_RANGE_1000_DEG: Serial.println("+-1000 deg/s"); break;
+    case MPU6050_RANGE_2000_DEG: Serial.println("+-2000 deg/s"); break;
+  }
+
+  mpu.setFilterBandwidth(MPU6050_BAND_21_HZ);
+  Serial.print("Filter bandwidth set to: ");
+  switch (mpu.getFilterBandwidth()) {
+    case MPU6050_BAND_260_HZ: Serial.println("260 Hz"); break;
+    case MPU6050_BAND_184_HZ: Serial.println("184 Hz"); break;
+    case MPU6050_BAND_94_HZ: Serial.println("94 Hz"); break;
+    case MPU6050_BAND_44_HZ: Serial.println("44 Hz"); break;
+    case MPU6050_BAND_21_HZ: Serial.println("21 Hz"); break;
+    case MPU6050_BAND_10_HZ: Serial.println("10 Hz"); break;
+    case MPU6050_BAND_5_HZ: Serial.println("5 Hz"); break;
+  }
+
+  Serial.println("Flex Sensors initialized. Remember to calibrate FLEX_MIN_STRAIGHT and FLEX_MAX_BENT values.");
+  Serial.println("------------------------------------");
+}
+
+unsigned long start = 0;
+unsigned long del = 0;
+
+void loop() {
+  sensors_event_t a, g, temp;
+  mpu.getEvent(&a, &g, &temp);
+  // Serial.print("ACCEL (m/s^2) - X: ");
+  // Serial.print(a.acceleration.x, 2);
+  // Serial.print(", Y: ");
+  // Serial.print(a.acceleration.y, 2);
+  // Serial.print(", Z: ");
+  // Serial.print(a.acceleration.z, 2);
+  // Serial.print(" | ");
+  // Serial.print("GYRO (rad/s) - X: ");
+  // Serial.print(g.gyro.x, 2);
+  // Serial.print(", Y: ");
+  // Serial.print(g.gyro.y, 2);
+  // Serial.print(", Z: ");
+  // Serial.print(g.gyro.z, 2);
+  // Serial.print(" | ");
+  // Serial.print("TEMP: ");
+  // Serial.print(temp.temperature, 2);
+  // Serial.print(" C | ");
+  // Serial.print("FLEX SENSORS (Raw/%) - ");
+  for (int i = 0; i < 5; i++) {
+    flexSensorValues[i] = analogRead(FLEX_PINS[i]);
+    int bendPercentage = map(flexSensorValues[i], FLEX_MIN_STRAIGHT, FLEX_MAX_BENT, 0, 100);
+    bendPercentage = constrain(bendPercentage, 0, 100);
+    // Serial.print("F");
+    // Serial.print(i + 1);
+    // Serial.print(": ");
+    // Serial.print(flexSensorValues[i]);
+    // Serial.print("/");
+    // Serial.print(bendPercentage);
+    // Serial.print("% ");
+  }
+  Serial.println();
+  if (WiFi.status() == WL_CONNECTED) {
+    http.begin(client, serverName);
+    http.addHeader("Content-Type", "application/json");
+    String json = "{";
+    json += "\"accel_x\":" + String(a.acceleration.x, 2) + ",";
+    json += "\"accel_y\":" + String(a.acceleration.y, 2) + ",";
+    json += "\"accel_z\":" + String(a.acceleration.z, 2) + ",";
+    json += "\"gyro_x\":" + String(g.gyro.x, 2) + ",";
+    json += "\"gyro_y\":" + String(g.gyro.y, 2) + ",";
+    json += "\"gyro_z\":" + String(g.gyro.z, 2) + ",";
+    json += "\"temperature\":" + String(temp.temperature, 2) + ",";
+    json += "\"flex_raw_1\":" + String(flexSensorValues[0]) + ",";
+    json += "\"flex_raw_2\":" + String(flexSensorValues[1]) + ",";
+    json += "\"flex_raw_3\":" + String(flexSensorValues[2]) + ",";
+    json += "\"flex_raw_4\":" + String(flexSensorValues[3]) + ",";
+    json += "\"flex_raw_5\":" + String(flexSensorValues[4]);
+    json += "}";
+    int httpResponseCode = http.POST(json);
+    if (httpResponseCode > 0) {
+      String response = http.getString();
+      Serial.println("Data sent to Google Sheets: " + response);
+    } else {
+      Serial.println("Error sending data. HTTP code: " + String(httpResponseCode));
+    }
+    http.end();
+  } else {
+    Serial.println("WiFi disconnected. Reconnecting...");
+    connectToWiFi();
+  }
+  delay(1000);
+}
--- server/app.py
+++ server/app.py
@@ -1,24 +1,72 @@
 from flask import Flask, request
 import pandas as pd
-for_excel = [["temperature","humidity","pressure"]]
+import numpy as np
 app = Flask(__name__)
+from tensorflow.keras.models import load_model
+model = load_model(f"M9-2-2025-ta/model.h5")
+with open(f"M9-2-2025-ta/text.txt", "r") as f:
+    class_names = f.read().splitlines()
 
-def to_excel():
-    global for_excel
-    
-    df = pd.DataFrame(for_excel)
-    df.to_csv("client_data.csv", mode="w", header=False)
-    
-    for_excel = [["temperature","humidity","pressure"]]
-    
+ldata = []
+rdata = []
+rc = False
+lc = False
 
-@app.route('/data', methods=['POST'])
-def data():
-    global for_excel
+def predict():
+    global ldata, rdata, rc, lc
+    rc = False
+    lc = False
+    forpredict = ldata + rdata
+    print(len(forpredict), "features")
+    df = np.array([forpredict]).reshape(1, -1)
+    pred = model.predict(df)
+    index = np.argmax(pred)
+    label = class_names[index]
+    print(label)
+
+
+@app.route('/right', methods=['POST'])
+def rdata():
+    global rdata, rc, lc
+    rc = True
     data = request.get_json()
-    temp = data["temperature"]
-    hum  = data["humidity"]
-    pres = data["pressure"]
+    raccel_x = data["accel_x"]
+    raccel_y = data["accel_y"]
+    raccel_z = data["accel_z"]
+    rgyro_x  = data["gyro_x"]
+    rgyro_y  = data["gyro_y"]
+    rgyro_z  = data["gyro_z"]
+    rtemp = data["temperature"]
+    rflex_1 = data["flex_raw_1"]
+    rflex_2 = data["flex_raw_2"]
+    rflex_3 = data["flex_raw_3"]
+    rflex_4 = data["flex_raw_4"]
+    rflex_5 = data["flex_raw_5"]
+    rdata = [raccel_x, raccel_y, raccel_z, rgyro_x, rgyro_y, rgyro_z, rtemp, rflex_1, rflex_2, rflex_3, rflex_4, rflex_5]
+    if lc and rc:
+        predict()
+    return "OK"
+
+@app.route('/left', methods=['POST'])
+def ldata():
+    global ldata, lc, rc
+    lc = True
+    data = request.get_json()
+    laccel_x = data["accel_x"]
+    laccel_y = data["accel_y"]
+    laccel_z = data["accel_z"]
+    lgyro_x  = data["gyro_x"]
+    lgyro_y  = data["gyro_y"]
+    lgyro_z  = data["gyro_z"]
+    ltemp = data["temperature"]
+    lflex_1 = data["flex_raw_1"]
+    lflex_2 = data["flex_raw_2"]
+    lflex_3 = data["flex_raw_3"]
+    lflex_4 = data["flex_raw_4"]
+    lflex_5 = data["flex_raw_5"]
+    ldata = [laccel_x, laccel_y, laccel_z, lgyro_x, lgyro_y, lgyro_z, ltemp, lflex_1, lflex_2, lflex_3, lflex_4, lflex_5]
+    if lc and rc:
+        predict()
     return "OK"
 
 app.run(host="0.0.0.0", port=5000)
